a414ea0926f5b93a650ff5957882dccb3001d46d
Elias <eliascamilo70@outlook.com>
2024-04-03 22:52:50 -0300

    attendee-list

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -5,15 +5,19 @@ import {
   ChevronsRight,
   MoreHorizontal,
   Search,
-} from 'lucide-react';
-import { IconButt } from './icon-button';
-import { Table } from './table/table';
-import { TableHeader } from './table/table-header';
-import { TableCell } from './table/table-cell';
-import { TableRow } from './table/table.row';
-import { ChangeEvent, useEffect, useState } from 'react';
-import { formatRelative } from 'date-fns';
-import { ptBR } from 'date-fns/locale';
+} from "lucide-react";
+import { IconButton } from "./icon-button";
+import { Table } from "./table/table";
+import { TableHeader } from "./table/table-header";
+import { TableCell } from "./table/table-cell";
+import { TableRow } from "./table/table-row";
+import { ChangeEvent, useEffect, useState } from "react";
+import dayjs from "dayjs";
+import "dayjs/locale/pt-br";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
+dayjs.locale("pt-br");
 
 interface Attendee {
   id: string;
@@ -23,42 +27,90 @@ interface Attendee {
   checkedInAt: string | null;
 }
 
-
 export function AttendeeList() {
-  const [valorDoInput, alterarValorInput] = useState('')
-  const [pagina, setPagina] = useState(1)
-  const [attendees, setAttendees] = useState([])
-  const totalPaginas = Math.ceil(attendees.length / 10)
+  const [search, setSearch] = useState(() => {
+    const url = new URL(window.location.toString());
+
+    if (url.searchParams.has("search")) {
+      return url.searchParams.get("search") ?? "";
+    }
+
+    return "";
+  });
+  const [page, setPage] = useState(() => {
+    const url = new URL(window.location.toString());
+
+    if (url.searchParams.has("page")) {
+      return Number(url.searchParams.get("page"));
+    }
+
+    return 1;
+  });
+
+  const [total, setTotal] = useState(0);
+  const [attendees, setAttendees] = useState<Attendee[]>([]);
+
+  const totalPages = Math.ceil(total / 10);
 
   useEffect(() => {
-    fetch('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      setAttendees(data.attendees)
-    })
+    const url = new URL(
+      "http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees"
+    );
 
-  }, [pagina])
+    url.searchParams.set("pageIndex", String(page - 1));
+    if (search.length > 1) {
+      url.searchParams.set("query", search);
+    }
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setAttendees(data.attendees);
+        setTotal(data.total);
+      });
+  }, [page, search]);
+
+  function setCurrentSearch(search: string) {
+    const url = new URL(window.location.toString());
+
+    url.searchParams.set("search", search);
+
+    window.history.pushState({}, "", url);
+
+    setSearch(search);
+  }
+
+  function setCurrentPage(page: number) {
+    const url = new URL(window.location.toString());
+
+    url.searchParams.set("page", String(page));
+
+    window.history.pushState({}, "", url);
+
+    setPage(page);
+  }
 
   function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
-    alterarValorInput(event.target.value)
-  }
-
-  function goToNextPage() {
-    setPagina(pagina + 1)
-  }
-
-  function goToPreviousPage() {
-    setPagina(pagina - 1)
+    setCurrentSearch(event.target.value);
+    setCurrentPage(1);
   }
 
   function goToFirstPage() {
-    setPagina(1)
+    setCurrentPage(1);
   }
 
   function goToLastPage() {
-    setPagina(totalPaginas)
+    setCurrentPage(totalPages);
   }
+
+  function goToPreviousPage() {
+    setCurrentPage(page - 1);
+  }
+
+  function goToNextPage() {
+    setCurrentPage(page + 1);
+  }
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex gap-3 items-center">
@@ -66,92 +118,105 @@ export function AttendeeList() {
         <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg flex items-center gap-3">
           <Search className="size-4 text-emerald-300" />
           <input
-            className="bg-transparent flex-1 outline-none border-0 p-0 text-sm"
+            className="bg-transparent focus:ring-0 flex-1 outline-none border-0 p-0 text-sm"
             placeholder="Buscar participante..."
+            value={search}
+            onChange={onSearchInputChanged}
           />
         </div>
-
-        {valorDoInput}
       </div>
 
-     <Table>
-          <thead>
-            <tr className="border-b border-white/10">
-              <TableHeader
-                style={{ width: 48 }}
-                
-              >
-                <input type="checkbox" className="size-4 bg-black/20 rounded border border-white/10" />
-              </TableHeader>
-              <TableHeader >Código</TableHeader>
-              <TableHeader >Participante</TableHeader>
-              <TableHeader >
-                Data de inscrição
-              </TableHeader>
-              <TableHeader >
-                Data do check-in
-              </TableHeader>
-              <TableHeader
-                style={{ width: 64 }}
-                
-              ></TableHeader>
-            </tr>
-          </thead>
-          <tbody>
-            {attendees.map((attendee) => {
-              return (
-                <TableRow key={attendee.id} >
-                  <TableCell >
-                    <input type="checkbox" className="size-4 bg-black/20 rounded border border-white/10" />
-                  </TableCell>
-                  <TableCell>{attendee.id}</TableCell>
-                  <TableCell>
-                    <div className="flex flex-col gap-1">
-                      <span className="font-semibold text-white">
-                        {attendee.name}
-                      </span>
-                      <span>{attendee.email}</span>
-                    </div>
-                  </TableCell>
-                  <TableCell>{attendee.createdAt}</TableCell>
-                  <TableCell>{attendee.checkedInAt}</TableCell>
-                  <TableCell>
-                    <IconButt transparent={true}>
-                      <MoreHorizontal className="size-4" />
-                    </IconButt>
-                  </TableCell>
-                </TableRow>
-              );
-            })}
-          </tbody>
-          <tfoot>
-            <tr>
-              <TableCell className="py-3 px-4 text-sm text-zinc-300" colSpan={3}>
-                Mostrando 10 de {attendees.length} itens
-              </TableCell>
-              <td className="py-3 px-4 text-sm text-zinc-300 text-right" colSpan={3}>
-                <div className="inline-flex items-center gap-8">
-                  <span>Página {pagina} de {totalPaginas}</span>
-
-                  <div className="flex gap-1.5">
-                    <IconButt onClick={goToFirstPage} disabled={pagina === 1}>
-                      <ChevronsLeft className="size-4" />
-                    </IconButt>
-                    <IconButt onClick={goToPreviousPage} disabled={pagina === 1}>
-                      <ChevronLeft className="size-4" />
-                    </IconButt>
-                    <IconButt onClick={goToNextPage} disabled={pagina === totalPaginas}>
-                      <ChevronRight className="size-4" />
-                    </IconButt>
-                    <IconButt onClick={goToLastPage} disabled={pagina === totalPaginas}>
-                      <ChevronsRight className="size-4" />
-                    </IconButt>
+      <Table>
+        <thead>
+          <tr className="border-b border-white/10">
+            <TableHeader style={{ width: 48 }}>
+              <input
+                type="checkbox"
+                className="size-4 bg-black/20 rounded border border-white/10"
+              />
+            </TableHeader>
+            <TableHeader>Código</TableHeader>
+            <TableHeader>Participante</TableHeader>
+            <TableHeader>Data de inscrição</TableHeader>
+            <TableHeader>Data do check-in</TableHeader>
+            <TableHeader style={{ width: 64 }}></TableHeader>
+          </tr>
+        </thead>
+        <tbody>
+          {attendees.map((ateendee) => {
+            return (
+              <TableRow key={ateendee.id}>
+                <TableCell>
+                  <input
+                    type="checkbox"
+                    className="size-4 bg-black/20 rounded border border-white/10"
+                  />
+                </TableCell>
+                <TableCell>{ateendee.id}</TableCell>
+                <TableCell>
+                  <div className="flex flex-col gap-1">
+                    <span className="font-semibold text-white">
+                      {ateendee.name}
+                    </span>
+                    <span>{ateendee.email}</span>
                   </div>
+                </TableCell>
+                <TableCell>{dayjs().to(ateendee.createdAt)}</TableCell>
+                <TableCell>
+                  {ateendee.checkedInAt === null ? (
+                    <span className="text-zinc-400">Não fez check-in</span>
+                  ) : (
+                    dayjs().to(ateendee.checkedInAt)
+                  )}
+                </TableCell>
+                <TableCell>
+                  <IconButton
+                    transparent
+                    className="bg-black/20 border border-white/10 rounded-md p-1.5"
+                  >
+                    <MoreHorizontal className="size-4" />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </tbody>
+        <tfoot>
+          <tr>
+            <TableCell colSpan={3}>
+              Mostrando {attendees.length} de {total} itens
+            </TableCell>
+            <TableCell className="text-right" colSpan={3}>
+              <div className="inline-flex items-center gap-8">
+                <span>
+                  Página {page} de {totalPages}
+                </span>
+
+                <div className="flex gap-1.5">
+                  <IconButton onClick={goToFirstPage} disabled={page === 1}>
+                    <ChevronsLeft className="size-4" />
+                  </IconButton>
+                  <IconButton onClick={goToPreviousPage} disabled={page === 1}>
+                    <ChevronLeft className="size-4" />
+                  </IconButton>
+                  <IconButton
+                    onClick={goToNextPage}
+                    disabled={page === totalPages}
+                  >
+                    <ChevronRight className="size-4" />
+                  </IconButton>
+                  <IconButton
+                    onClick={goToLastPage}
+                    disabled={page === totalPages}
+                  >
+                    <ChevronsRight className="size-4" />
+                  </IconButton>
                 </div>
-              </td>
-            </tr>
-          </tfoot>
-          </Table>
+              </div>
+            </TableCell>
+          </tr>
+        </tfoot>
+      </Table>
     </div>
   );
 }
